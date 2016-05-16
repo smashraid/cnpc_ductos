3769caa4fefbbf315077d7fdc04f539bc1263fed
Jose Liza <joselizag@gmail.com>
2016-05-16 02:08:27 -0500

    Actualizacion de registros oleoductos gaseoductos y tuberias

--- js/scripts.js
+++ js/scripts.js
@@ -47,14 +47,14 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+//span.onclick = function() {
+//    modal.style.display = "none";
+//}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
